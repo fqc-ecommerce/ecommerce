d0a4785e6fe0dd8e08db8fa2278a5d5366c90c9a
francoqueirolo <fgqueirolo@gmail.com>
2026-01-09 00:00:50 -0500

    chore: first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
+      </p>
+    </>
+  );
+}
+
+export default App;
